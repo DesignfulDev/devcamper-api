8bf69d452e2fa066063ded2e688ac172bd1fd9d3
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-01-30 17:57:29 -0300

    Initial Express setup

---
+++ server.js
@@ -0,0 +1,48 @@
+// Load core modules
+
+// Load 3rd party modules
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+// Load enviroment variables
+dotenv.config({ path: './config/config.env' });
+
+// Load custom modules
+const connectDB = require('./config/db');
+
+// Connect to database
+connectDB();
+
+// Load router files
+
+// Create Express server
+const app = express();
+
+// Use body parser
+
+// Use developer loggin middleware
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Use middleware methods
+
+// Use routers
+
+// Define PORT
+const PORT = process.env.PORT || 5000;
+
+// Fire up the Server
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+});
+
+// Handle Promise Rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
