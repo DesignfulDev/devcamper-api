d878e671a99cbeb93cc994f8eb2ea0a705c284a9
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-01-30 19:54:54 -0300

    Adds try-catch block

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -21,11 +21,15 @@ exports.getBootcamp = (req, res, next) => {
 // @route     POST /api/v1/bootcamps
 // @access    Private
 exports.createBootcamp = async (req, res, next) => {
-  const bootcamp = await Bootcamp.create(req.body);
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
 
-  res
-    .status(200)
-    .json({ success: true, msg: `Create new bootcamp`, data: bootcamp });
+    res
+      .status(200)
+      .json({ success: true, msg: `Create new bootcamp`, data: bootcamp });
+  } catch (err) {
+    res.status(500).json({ success: false, msg: err.message });
+  }
 };
 
 // @desc      Update bootcamp
