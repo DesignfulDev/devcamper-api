e8cff7003a647778f81a53f6578da925fe412e3f
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-01-30 17:40:11 -0300

    Adds deprecation options

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   console.log(
     `Successfully connected to ${conn.connection.db.databaseName} at ${conn.connection.host}`
