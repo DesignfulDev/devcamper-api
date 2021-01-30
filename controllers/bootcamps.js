34fc19ac571027e0c56d19ad47c9613d8c2009cc
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-01-30 18:33:35 -0300

    Changes response method to status

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,14 +2,14 @@
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = (req, res, next) => {
-  res.send(200).json({ success: true, msg: `Show all bootcamps` });
+  res.status(200).json({ success: true, msg: `Show all bootcamps` });
 };
 
 // @desc      Show single bootcamp by id
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
 exports.getBootcamp = (req, res, next) => {
-  res.send(200).json({
+  res.status(200).json({
     success: true,
     msg: `Show single bootcamp by id ${req.params.id}`,
   });
@@ -18,24 +18,24 @@ exports.getBootcamp = (req, res, next) => {
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Private
-exports.getBootcamps = (req, res, next) => {
-  res.send(200).json({ success: true, msg: `Create new bootcamp` });
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Create new bootcamp` });
 };
 
 // @desc      Update bootcamp
 // @route     PUT /api/v1/bootcamps/:id
 // @access    Private
-exports.getBootcamps = (req, res, next) => {
+exports.updateBootcamp = (req, res, next) => {
   res
-    .send(200)
+    .status(200)
     .json({ success: true, msg: `Update bootcamp by id ${req.params.id}` });
 };
 
 // @desc      Delete bootcamp
 // @route     DELETE /api/v1/bootcamps/:id
 // @access    Private
-exports.getBootcamps = (req, res, next) => {
+exports.deleteBootcamp = (req, res, next) => {
   res
-    .send(200)
+    .status(200)
     .json({ success: true, msg: `Delete bootcamp by id ${req.params.id}` });
 };
