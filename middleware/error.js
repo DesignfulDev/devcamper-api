016e70834dee8664113fd693d0502b3b0389fa26
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-01-31 16:39:27 -0300

    Adds duplicate value error handler

--- middleware/error.js
+++ middleware/error.js
@@ -6,12 +6,21 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   // Log error for dev
+  console.log(err.name);
   console.log(err.stack.red);
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource not found with id ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongo duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate value for property '${Object.keys(
+      err.keyValue
+    )}: ${Object.values(err.keyValue)}'`;
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
