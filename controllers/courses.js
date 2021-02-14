f85a02573b0cbb09106004f8286aaa92fe0647f4
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-02-14 17:10:42 -0300

    Adds GET single course by id

--- controllers/courses.js
+++ controllers/courses.js
@@ -27,3 +27,22 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     data: courses,
   });
 });
+
+// @desc      Show single course by ID
+// @route     GET /api/v1/courses/:id
+// @access    Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course not found with id ${req.params.id}`),
+      404
+    );
+  }
+
+  res.status(200).json({ success: true, data: course });
+});
