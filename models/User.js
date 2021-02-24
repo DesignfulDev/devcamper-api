eaf7806180e46df437ea7eee81c39f7cddd17eac
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-02-24 17:25:19 -0300

    Creates User model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    maxlength: [50, 'Name cannot be longer than 50 characters'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an e-mail'],
+    unique: [true, 'E-mail already exists'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'publisher'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: [8, 'Password cannot be less than 8 characters'],
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
