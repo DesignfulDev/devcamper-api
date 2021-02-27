53f95819f0864d28c2e0d1327bccea998ab12421
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-02-27 01:10:32 -0300

    Adds protected route to get current logged in user

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,3 +46,12 @@ exports.login = asyncHandler(async (req, res, next) => {
 
   sendTokenResponse(user, 200, res);
 });
+
+// @desc      Show current logged in user
+// @route     POST /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({ success: true, data: user });
+});
