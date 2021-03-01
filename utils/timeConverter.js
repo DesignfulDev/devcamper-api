65f628de2c2685876184b12c215345e9abb73c0a
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-03-01 14:31:23 -0300

    Adds more functionality to time converter

--- utils/timeConverter.js
+++ utils/timeConverter.js
@@ -1,5 +1,34 @@
-const daysToMs = days => {
-  return days * 24 * 60 * 60 * 1000;
+exports.toMilliseconds = (time, from) => {
+  console.log('Running time converter...');
+  switch (from) {
+    case 'seconds':
+      return time * 1000;
+    case 'minutes':
+      return time * 60 * 1000;
+    case 'hours':
+      return time * 60 * 60 * 1000;
+    case 'days':
+      return time * 24 * 60 * 60 * 1000;
+    case 'years':
+      return time * 365.25 * 24 * 60 * 60 * 1000;
+    default:
+      throw Error('Time conversion function is missing parameters');
+  }
 };
 
-module.exports = daysToMs;
+exports.fromMilliseconds = (time, to) => {
+  switch (to) {
+    case 'seconds':
+      return time / 1000;
+    case 'minutes':
+      return time / 60 / 1000;
+    case 'hours':
+      return time / 60 / 60 / 1000;
+    case 'days':
+      return time / 24 / 60 / 60 / 1000;
+    case 'years':
+      return time / 365.25 / 24 / 60 / 60 / 1000;
+    default:
+      throw Error('Time conversion function is missing parameters');
+  }
+};
