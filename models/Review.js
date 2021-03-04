5c38f552a4cc88dc7ab4b9cb093bbc2290e89c08
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-03-04 17:55:23 -0300

    Creates Review data model

---
+++ models/Review.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please provide a title for the review'],
+    maxlength: [100, 'Review title cannot exceed 100 characters'],
+  },
+  text: {
+    type: String,
+    required: [true, 'Please provide a text for the review'],
+    maxlength: [500, 'Review text cannot exceed 500 characters'],
+  },
+  rating: {
+    type: Number,
+    required: [true, 'Please provide a rating for the review'],
+    min: [1, 'Rating cannot be less than 1'],
+    max: [10, 'Rating cannot be more than 10'],
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: [true, 'The review needs to be associated with a bootcamp'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'The review needs to be associated with a user'],
+  },
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
