2b15a1681900266096857ea045ad92c6c845b046
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-03-06 22:57:24 -0300

    Adds feature to update reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -67,3 +67,33 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+// @desc      Update review
+// @route     PUT /api/v1/reviews/:id
+// @access    Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review found with id ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user  owns the review or is an admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    msg: 'Review updated',
+    updatedFields: req.body,
+    data: review,
+  });
+});
