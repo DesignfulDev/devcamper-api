173d173adac63b1dd2a9e16bcfa62ddbfec0f5f3
Mauricio Lacerda <mauricio.lacerda@pm.me>
2021-03-07 21:40:06 -0300

    Adds XSS attack protection

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 
 // Load enviroment variables
 dotenv.config({ path: './config/config.env' });
@@ -48,6 +49,9 @@ app.use(mongoSanitize());
 
 // Set security headers
 app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
